9b263db017ac4e593d017d2525b21c33f1939817
Iain Laird <iain@laird.org>
2022-04-21 01:18:39 +0100

    Update piet-common

--- druid/examples/sub_window.rs
+++ druid/examples/sub_window.rs
@@ -17,6 +17,7 @@
 
 use druid::commands::CLOSE_WINDOW;
 use druid::lens::Unit;
+use druid::piet::{TextLayoutBuilder, TextLayout};
 use druid::widget::{
     Align, Button, Checkbox, Controller, ControllerHost, EnvScope, Flex, Label, TextBox,
 };
@@ -28,7 +29,6 @@ use druid::{
 use druid_shell::piet::Text;
 use druid_shell::{Screen, WindowLevel};
 use instant::{Duration, Instant};
-use piet_common::{TextLayout, TextLayoutBuilder};
 
 const VERTICAL_WIDGET_SPACING: f64 = 20.0;
 const TEXT_BOX_WIDTH: f64 = 200.0;
